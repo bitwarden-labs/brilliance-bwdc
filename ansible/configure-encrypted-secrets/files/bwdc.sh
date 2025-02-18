f316a78891b53c80aeef78d1d364df494a277a41
Adam Thomas Jagger Bramley <atjbramley@hotmail.com>
2025-02-18 11:45:06 +0100

    update logging target, remove duplicate unlock

--- ansible/configure-encrypted-secrets/files/bwdc.sh
+++ ansible/configure-encrypted-secrets/files/bwdc.sh
@@ -3,7 +3,7 @@
 # Set variables
 PASSWORD=$(bws secret get 'a44294c3-a73e-4761-ac12-b286011e62d2' | jq -r '.value')
 LDAP_PASSWORD=$(bws secret get 'e8d8b83f-06ee-4e11-926f-b286011f5a21' | jq -r '.value')
-LOG_DIR="/var/log/bwdc"
+LOG_DIR="/home/bwdc/logs"
 LOG_FILE="${LOG_DIR}/bwdc_test_$(date +%Y-%m-%d).log"
 
 # Ensure the log directory exists
@@ -11,11 +11,6 @@ mkdir -p "$LOG_DIR"
 chmod 755 "$LOG_DIR"
 
 # Start dbus and gnome-keyring-daemon
-export $(dbus-launch)
-gnome-keyring-daemon --start --daemonize --components=secrets
-echo "$PASSWORD" | gnome-keyring-daemon -r -d --unlock
-
-# Re-export dbus and restart gnome-keyring-daemon
 export $(dbus-launch)
 gnome-keyring-daemon --start --daemonize --components=secrets
 echo "$PASSWORD" | gnome-keyring-daemon -r -d --unlock
